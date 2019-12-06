78da2f49cd6f94b26426330891141549686425a8
氏家翔吾 <ujiieshougo@mm-dhcp-125-012.naist.jp>
2019-12-06 17:53:22 +0900

    カルマンスムーシングの実装

--- KF.py
+++ KF.py
@@ -1,6 +1,24 @@
 import numpy as np
 from scipy.stats import norm
 
+def smoothing(mu, P, V):
+    res_mu = []
+    res_V = []
+    res_mu.append(mu[0])
+    res_V.append(V[0])
+
+    for i in range(1, len(mu)):
+        J = V[i] / P[i-1]
+        print("J", J)
+        res_mu.append(mu[i] + J * (res_mu[-1] - mu[i]))
+        res_V.append(V[i] + J * (res_V[-1] - P[i-1]) * J)
+    
+    res_mu = res_mu[::-1]
+    res_V = res_V[::-1]
+
+    return res_mu, res_V
+
+
 def update_parameter(x, sigma, eta, mu, P, log_likelihood):
     if log_likelihood is None:
         _mu_prev = mu
@@ -10,7 +28,7 @@ def update_parameter(x, sigma, eta, mu, P, log_likelihood):
     else:
         _mu_prev = mu
         _P_prev = P + eta
-        _x = np.log(norm.pdf(x, _mu_prev, _P_prev + sigma))
+        _x = np.log(norm.pdf(x, _mu_prev, np.sqrt(_P_prev + sigma)))
         _log_likelihood =  _x + log_likelihood
 
 
@@ -37,15 +55,40 @@ eta = 10
 mu = 100
 P = 50
 
+mu_list = []
+P_list = []
+V_list = []
+
 mu_prev, P_prev, log_likelihood, x, K, mu, P = update_parameter(INPUT_X[0], sigma, eta, mu, P, None)
+mu_list.append(mu)
+P_list.append(P_prev)
+V_list.append(P)
 print("problem 1:")
 print("log_likelihood = {}\nK = {}\nmu = {}\nP = {}\n".format(log_likelihood, K, mu, P))
 mu_prev, P_prev, log_likelihood, x, K, mu, P = update_parameter(INPUT_X[1], sigma, eta, mu, P, log_likelihood)
+mu_list.append(mu)
+P_list.append(P_prev)
+V_list.append(P)
 print("problem 2:")
 print("mu_p = {}\nP_prev = {}\nlog_likelihood = {}\nmarginal = {}\nK = {}\nmu = {}\nP = {}\n".format(mu_prev, P_prev, log_likelihood, x, K, mu, P))
 mu_prev, P_prev, log_likelihood, x, K, mu, P = update_parameter(INPUT_X[2], sigma, eta, mu, P, log_likelihood)
+mu_list.append(mu)
+P_list.append(P_prev)
+V_list.append(P)
 print("problem 3:")
 print("mu_p = {}\nP_prev = {}\nlog_likelihood = {}\nmarginal = {}\nK = {}\nmu = {}\nP = {}\n".format(mu_prev, P_prev, log_likelihood, x, K, mu, P))
 mu_prev, P_prev, log_likelihood, x, K, mu, P = update_parameter(INPUT_X[3], sigma, eta, mu, P, log_likelihood)
+mu_list.append(mu)
+P_list.append(P_prev)
+V_list.append(P)
 print("problem 4:")
 print("mu_p = {}\nP_prev = {}\nlog_likelihood = {}\nmarginal = {}\nK = {}\nmu = {}\nP = {}\n".format(mu_prev, P_prev, log_likelihood, x, K, mu, P))
+
+mu_list = mu_list[::-1]
+P_list = P_list[::-1]
+V_list = V_list[::-1]
+
+print("mu", mu_list)
+print('P', P_list)
+print('V', V_list)
+print(smoothing(mu_list, P_list, V_list))
--- NN.py
+++ NN.py
@@ -2,8 +2,6 @@ import numpy as np
 
 class NN(object):
     def __init__(self, W1, W2, l):
-        self.b_1 = np.zeros((2,1))
-        self.b_2 = np.zeros((1,1))
         self.W1 = W1
         self.W2 = W2
         self.u1 = 0
@@ -13,37 +11,32 @@ class NN(object):
         self.l = l
 
     def update(self, y):
-        w1_grad, w2_grad, b1_grad, b2_grad = self._backward(y)
+        w1_grad, w2_grad = self._backward(y)
         self.W1 -= self.l * w1_grad
         self.W2 -= self.l * w2_grad.T
-        self.b_1 -= self.l * b1_grad
-        self.b_2 -= self.l * b2_grad
         print('W1', self.W1)
         print('W2', self.W2)
-        print("b_1", self.b_1)
-        print("b_2", self.b_2)
 
     def forward(self, X):
         self.input = X
-        self.u1 = self.W1 @ X + self.b_1
+        self.u1 = self.W1 @ X
         self.z1 = np.tanh(self.u1)
-        self.u2 = (self.W2 @ self.z1 + self.b_2)[0,0]
+        self.u2 = (self.W2 @ self.z1)[0,0]
         self.output = np.tanh(self.u2)
 
         return self.output
 
     def _backward(self, y):
         delta_2 = (self.output - y) * (1 - self.u2 * self.u2)
-        print('delta2', delta_2)
         w2_grad = delta_2 * self.z1
-        b2_grad = delta_2
 
         delta_1 = delta_2 * self.W2.T * (np.ones((2,1)) - self.u1 * self.u1)
-        print('delta1', delta_1)
         w1_grad = np.tile(self.input.T, (2,1)) * np.tile(delta_1, (1,2))
-        b1_grad = delta_1
 
-        return w1_grad, w2_grad, b1_grad, b2_grad
+        print(w1_grad)
+        print(w2_grad)
+
+        return w1_grad, w2_grad
 
 
 STUDENT_ID = input("please, input a your id:")
@@ -61,16 +54,12 @@ w1[1,1] = (b_3 + 1) / 10
 w2[0,0] = (b_1 + 1) / 10
 w2[0,1] = - (b_2 + 1) / 10 
 
-model = NN(w1, w2, 0.2)
+model = NN(w1, w2, 1)
 print(model.forward(np.array([[1], [1]])))
 model.update(1)
-print(model.forward(np.array([[1], [1]])))
 print(model.forward(np.array([[0], [1]])))
 model.update(-1)
-print(model.forward(np.array([[0], [1]])))
-print(model.forward(np.array([[1], [0]])))
+print(model.forward(np.array([[0], [0]])))
 model.update(1)
 print(model.forward(np.array([[1], [0]])))
-print(model.forward(np.array([[0], [0]])))
 model.update(-1)
-print(model.forward(np.array([[0], [0]])))
